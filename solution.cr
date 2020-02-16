5eb536431e8b32a343d8558e714e36ce80efb0e7
Nameer <nameer6g6@gmail.com>
2020-02-16 12:55:04 +0300

    modified solution up to max score

--- solution.cr
+++ solution.cr
@@ -1,4 +1,4 @@
-inputs = ["./input/a_example.in", "./input/b_small.in", "./input/c_medium.in", "./input/d_quite_big.in", "./input/e_also_big.in"]
+inputs = ["./input_files/a_example.in", "./input_files/b_small.in", "./input_files/c_medium.in", "./input_files/d_quite_big.in", "./input_files/e_also_big.in"]
 inputs.each do |in|
   file_name = in.split("/")[2].split(".")[0]
   x = File.read(in).split("\n")
@@ -11,25 +11,38 @@ inputs.each do |in|
   length = int_ara.size - 1
 
   ara_collector = Array(Array(Int32)).new
+  ara_indx_collector = Array(Array(Int32)).new
   maximum_ara = 0
 
-  length.downto(0) do |l|
-    ara_counter = 0
+  (0).upto(length) do |l|
+    ara_upper_counter = 0
     ara = Array(Int32).new
-    l.downto(0).each do |i|
-      break if ara.size >= max_types
-      if int_ara[i] + ara_counter <= max_slices
-        ara.unshift(i)
-        ara_counter += int_ara[i]
+    ara_indx = Array(Int32).new
+    (length).downto(0).each do |i|
+      next if l == i
+      if ara_upper_counter + int_ara[i] <= max_slices && ara_upper_counter <= max_slices
+        ara.unshift(int_ara[i])
+        ara_indx.unshift(i)
+        ara_upper_counter += int_ara[i]
       end
     end
-    if ara_counter > maximum_ara && ara_counter <= max_slices
-      maximum_ara = ara_counter
+    if ara_upper_counter <= max_slices
       ara_collector << ara
+      ara_indx_collector << ara_indx
     end
   end
 
-  ara_ara = ara_collector[-1]
+  ara = Array(Int32).new
+  ara_index = 0
+  ara_num = 0
+  ara_collector.each.with_index do |v, i|
+    if v.sum > ara_num && v.sum >= max_slices
+      ara_index = i
+      ara_num = v.sum
+    end
+  end
+
+  ara_ara = ara_indx_collector[ara_index]
 
   File.open("./results/#{file_name}.result", "w") do |f|
     f.puts ara_ara.size.to_s
